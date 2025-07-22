fab16d07cc8a69c73e2aec9dad4864f206541c2a
jann83 <83481345+jann83@users.noreply.github.com>
2025-07-22 17:19:57 -0600

    segundo commit del proyecto TES

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from '@/components/hero'
 import Features from '@/components/features'
-import Newsletter from '@/components/newsletter'
 import Zigzag from '@/components/zigzag'
 import Testimonials from '@/components/testimonials'
 import Blocks from '@/components/blocks'
